5adba8d4d13a800cb715c8a60efd945bdabda93f
Miguel Bedoya <me.bedoya10@gmail.com>
2020-08-05 16:17:00 -0500

    refactoring: creation of a score function. Missing return the final result

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
+
 const resultWin = 'You WIN!';
 const resultLost = 'You LOST :)';
 const resultTie = 'There is a tie.';
 
-const rock = 'rock';
-const paper = 'paper';
-const scissors = 'scissors';
+const userWin = 'User is the winner';
+const machineWinner = 'Machine is the winner';
+const tie = 'The match is tie';
+
+const rounds = 5;
 
 /*
 A HandSign is one of those:
@@ -15,34 +21,26 @@ Interpretation: each HandSign corresponds to the element
 */
 
 /*
-Number -> HandSign
+__ -> HandSign
 When it is invoke, use random and floor to get a whole random number, between
-0 until Number-1.
-given: 3, expect: scisorss
-given: 3, expect: rock
-given: 3, expect: paper
-function computerPlay( options ) { ... }
+0 until 3, inluding both limits. Finally translate the number into a HandSign.
 */
 function computerPlay() {
     let numberRandom = Math.floor( Math.random() * 3 );
-    switch ( numberRandom ) {
-        case 0:
-        return rock;
-        break;
-        case 1:
-        return paper;
-        break;
-        case 2:
-        return scissors;
-        break;
-        // default:
-    }
+
+    if ( numberRandom == 0 ) return rock;
+    if ( numberRandom == 1 ) return paper;
+    if ( numberRandom == 2 ) return scissors;
 }
 /*
-Number ->
+__ -> HandSign
+Ask to user enter a HandSign with prompt, and use toLowerCase to make function
+case insensitive.
 */
 function userPlay() {
-    if
+    let userEntry =  prompt("Please select one of this:\nRock\nPaper\nScissors");
+
+    return userEntry.toLowerCase();
 }
 /*
 HandSign HandSign -> String
@@ -74,33 +72,66 @@ function playSingleRound( playerSelection, computerSelection ) {
         return resultLost;
     } else return "No valid";
 }
+/*
+String -> String
+Keeps score and reports a winner or loser at the end of 5 rounds.
+given: resultWin, resultWin, resultWin, resultLost, resultLost; expect: userWin
+given: resultWin, resultLost, resultWin, resultLost, resultLost; expect: machineWin
+function score( result ) {...}
+*/
+function score( str ) {
+    let userScore = 0;
+    let machineScore = 0;
+    let i = 0;
+
+    if ( str === resultWin ) ++userScore;
+    if ( str === resultLost ) ++machineScore;
+    ++i;
+
+    if ( i == rounds - 1 ) {
+        if ( userScore > machineScore ) {
+            return userWin;
+        } else if ( userScore < machineScore ) {
+            return machineWin;
+        } else return tie;
+    }
+}
 
 function game() {
 
-    let userScore = 0;
-    let machineScore = 0;
+    for ( let i = 0; i < rounds; ++i ) {
+        let result = playSingleRound( userPlay(), computerPlay() );
 
-    for ( let i = 0; i < 5; ++i ) {
-        let userChoice = prompt("Welcome to game: Rock, Paper & Scissors\n\nPlease enter your handsign: ");
-        if ( userChoice == null ) {
-            console.log("You did not enter any.");
-            --i;
-        } else {
-            let machineChoice = computerPlay( 3 );
-            let result = playSingleRound( userChoice.toLowerCase().trim(), machineChoice );
-            console.log(result);
-            if ( result == resultWin ) {
-                ++userScore;
-            } else if ( result == resultLost ) {
-                ++machineScore;
-            } else if ( result == "No valid" ) --i;
-        }
+        console.log( result );
+        score( result );
     }
-    if ( userScore > machineScore ) {
-        return "User is the winner"
-    } else if ( userScore < machineScore ) {
-        return "Machine is the winner"
-    } else return "The game is tied"
+
+
+
+    // let userScore = 0;
+    // let machineScore = 0;
+
+    // for ( let i = 0; i < 5; ++i ) {
+    //     let userChoice = prompt("Welcome to game: Rock, Paper & Scissors\n\nPlease enter your handsign: ");
+    //     if ( userChoice == null ) {
+    //         console.log("You did not enter any.");
+    //         --i;
+    //     } else {
+    //         let machineChoice = computerPlay( 3 );
+    //         let result = playSingleRound( userChoice.toLowerCase().trim(), machineChoice );
+    //         console.log(result);
+    //         if ( result == resultWin ) {
+    //             ++userScore;
+    //         } else if ( result == resultLost ) {
+    //             ++machineScore;
+    //         } else if ( result == "No valid" ) --i;
+    //     }
+    // }
+    // if ( userScore > machineScore ) {
+    //     return "User is the winner"
+    // } else if ( userScore < machineScore ) {
+    //     return "Machine is the winner"
+    // } else return "The game is tied"
 }
 
-playSingleRound( userPlay(), computerPlay() )
+game();
